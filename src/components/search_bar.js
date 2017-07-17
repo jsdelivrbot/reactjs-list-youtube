fbd00f5c4564a7410d38cc992fafb878e3b6b23c
zakaria <zakaria@concord-consulting.com>
2017-07-17 15:02:48 +0700

    zacka

---
+++ src/components/search_bar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+//const Component = React.Component;
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { nilai: 'nilai awal' };
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.nilai}
+          onChange={event => this.setState({ nilai: event.target.value })} />
+        {/* Value of the input: {this.state.nilai} */}
+      </div>
+    );
+  }
+
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
+}
+
+export default SearchBar;
---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyBPo_Q4Xnzy6LDeOCfF9zDGhyWl_DxCJgI';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+      //this.setState({})
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+// Take this component's generated HTML and put it
+// on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
