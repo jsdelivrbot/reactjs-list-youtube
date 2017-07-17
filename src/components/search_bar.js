0bb9f2d5af6947155e7a28e9db7fdd3831cee8c9
zakaria <zakaria@concord-consulting.com>
2017-07-17 16:05:35 +0700

    s

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,20 +5,24 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { nilai: 'nilai awal' };
+    this.state = { term: '' };
   }
 
   render() {
     return (
       <div className="search-bar">
         <input
-          value={this.state.nilai}
-          onChange={event => this.setState({ nilai: event.target.value })} />
-        {/* Value of the input: {this.state.nilai} */}
+          value={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)} />
+        {/* Value of the input: {this.state.term} */}
       </div>
     );
   }
 
+  onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
   // onInputChange(event) {
   //   console.log(event.target.value);
   // }
