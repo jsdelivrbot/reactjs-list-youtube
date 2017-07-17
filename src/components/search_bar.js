4580b1382a13b23e4eb53fc1b7166a6ea6aed1f2
zakaria <zakaria@concord-consulting.com>
2017-07-17 15:10:39 +0700

    fs

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,7 +10,7 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input
           value={this.state.nilai}
           onChange={event => this.setState({ nilai: event.target.value })} />
